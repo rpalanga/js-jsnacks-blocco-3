e75409e713acfbe100189274d11a9eef3f1729f3
roberto-palanga <r.palanga94@gmail.com>
2024-02-12 14:14:34 +0100

    Inizio secondo esercizio

---
+++ jsnack2/script.js
@@ -0,0 +1,12 @@
+
+/*
+Snack 2:
+Scrivi una funzione che fonda due array presi come parametri 
+
+(date per scontato che abbiano lo stesso numero di elementi)
+
+ prendendo alternativamente gli elementi da uno e dall’altro
+
+es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+*/
+
