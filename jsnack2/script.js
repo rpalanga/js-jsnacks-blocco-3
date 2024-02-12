bf34d995a98c035a42f93b0e9773c9ef56fdb1b8
roberto-palanga <r.palanga94@gmail.com>
2024-02-12 17:03:42 +0100

    Secondo snack finito

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -10,3 +10,24 @@ Scrivi una funzione che fonda due array presi come parametri
 es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
 */
 
+let firstArray = [0,1,2,3];
+let secondArray = [`a`,`b`,`c`,`d`];
+let thirdArray =[];
+
+const mixedArrey = function (firstA, secondA){
+
+   
+    for (let i = 0; i < firstArray.length; i++){
+
+        thirdArray.push(firstA[i]);
+        thirdArray.push(secondA[i]);
+        
+    }
+    return thirdArray;
+};
+
+
+let result = mixedArrey(firstArray, secondArray)
+console.log(result)
+
+document.getElementById("result").innerHTML = `Il risultato è questo ${result}`
