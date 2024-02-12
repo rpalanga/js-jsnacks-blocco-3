68e548ea6c97e6860d4e6ba632f7545f0b113a51
roberto-palanga <r.palanga94@gmail.com>
2024-02-12 18:19:10 +0100

    esercizio tre snack finito

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -6,3 +6,27 @@ un array e due numeri (“a” più piccolo di “b” e “b” grande al massi
 
 */
 
+let numberArray = [0,1,2,3,4,5,6];
+
+
+const rangeArray = function (array , min , max){
+
+    let mixArray = [];
+    
+
+    for( let i = min; i <= max; i++){
+
+        mixArray.push(array[i])
+
+    }
+
+    return mixArray;
+};
+
+let newArray = rangeArray(numberArray, 2, 5)
+console.log(newArray)
+
+document.getElementById("result").innerHTML = `Il numero degli elementi è ${newArray}`
+
+// console.log(rangeArray(mixArrry, 2,5))
+
